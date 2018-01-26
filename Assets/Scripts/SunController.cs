bf42979a30a7e1ed9ac39add4b72da4f552f516f
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2018-01-26 20:38:59 +0100

    Added basic sun movement.

--- Assets/Scripts/SunController.cs
+++ Assets/Scripts/SunController.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SunController : MonoBehaviour {
+public class SunController : MonoBehaviour
+{
 
-	public float moveSpeed;
+	public float moveSpeed = 0.25f;
 
+	public LevelBoundaries boundaries;
 
+	void Update ()
+	{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		float dir = 0;
+		if (Input.GetKey (KeyCode.LeftArrow))
+			dir = -1;
+		else if (Input.GetKey (KeyCode.RightArrow))
+			dir = 1;
+
+		if (dir != 0) {
+			float movement = dir * moveSpeed * Time.deltaTime;
+
+			float tentativePosition = transform.position.x + movement;
+
+			var oldPos = transform.position;
+			transform.position = new Vector3(Mathf.Clamp (boundaries.left, tentativePosition, boundaries.right), oldPos.y, oldPos.z);
+		}
+
 	}
 }
