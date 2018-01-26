a08cab46690b2b69493990fb04a5e78a950974b0
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2018-01-26 22:21:29 +0100

    Change light color based on remaining energy.

--- Assets/Scripts/EnergyEmitter.cs
+++ Assets/Scripts/EnergyEmitter.cs
@@ -8,17 +8,28 @@ public class EnergyEmitter : MonoBehaviour {
 
 	float remainingEnergy;
 
+	public Light light;
+
+	Color initialColor;
+
+	Color finalColor = Color.red;
+
 	void Start () {
 		remainingEnergy = initialEnergy;
+		initialColor = light.color;
 	}
 
-	void Update() {
-		
+	void UpdateColor() {
+		float percentage = remainingEnergy / initialEnergy;
+		light.color = Color.Lerp (finalColor, initialColor, percentage);
 	}
 
 	public float TransmitEnergy(float demand) {
 		float provided = Mathf.Min (remainingEnergy, demand);
 		remainingEnergy = Mathf.Max (0, remainingEnergy - provided);
+
+		UpdateColor ();
+
 		return provided;
 	}
 }
